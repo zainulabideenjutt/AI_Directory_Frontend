8cc13866068af632acdd64729f4eb50a34ad6b97
Zain Ul Abideen <zainulabideen.jutt1@gmail.com>
2025-06-19 16:09:34 +0500

    changed image Paths

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,11 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: process.env.NODE_ENV === 'development' ? 'http' : 'https',
-        hostname: process.env.NODE_ENV === 'development' ? '127.0.0.1' : 'your-api-domain.com',
-        port: process.env.NODE_ENV === 'development' ? '8000' : '',
+        protocol: 'https',
+        hostname: 'aixploria.com',
         pathname: '/**',
       },
     ],
-  },
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
 };
 
